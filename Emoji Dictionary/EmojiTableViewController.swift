e99acf8365d65ddf112923a83f6c7931ce7e6027
Rob McDonnell <rob.f.mcdonnell@outlook.com>
2017-09-28 22:47:45 -0400

    Initial commit

---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Rob McDonnell on 9/28/17.
+//  Copyright © 2017 Rob McDonnell. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}

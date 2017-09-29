a0eac2b4201f9b688534bac99783bd335750e9f2
Rob McDonnell <rob.f.mcdonnell@outlook.com>
2017-09-28 23:26:02 -0400

    Inserted emojis into table view

--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
 
+    var emojis = ["🐶", "🐱", "🦊", "🦁", "🐷", "🐸", "🐵", "🐧", "🐥", "🦅"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return emojis.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
 
-        // Configure the cell...
-
+        cell.textLabel?.text = emojis[indexPath.row]
+        
         return cell
     }
 
